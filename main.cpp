33104249744e3e541d46b97ad176e47bcf5e46cc
Eyad Arshad <eyadyr7866@gmail.com>
2025-04-07 21:13:40 +0500

    eyad

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QPushButton>
+#include <QStringList>
+#include <QJSEngine>  // For evaluating math expressions
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
     return a.exec();
 }
+
--- mainwindow.h
+++ mainwindow.h
@@ -4,9 +4,7 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -14,10 +12,11 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
 };
+
 #endif // MAINWINDOW_H
